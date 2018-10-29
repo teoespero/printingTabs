4d2d079f8779706292a77837e840bb6cfe1a0a16
Teo Espero <38012835+teoespero@users.noreply.github.com>
2018-10-29 16:01:06 -0700

    Add files via upload

---
+++ raceHorses.py
@@ -0,0 +1,13 @@
+# Teo Espero
+# BS Cloud and Systems Administration
+# Western Governors University
+
+print("Race Horses who won the Kentucky Derby")
+print("--------------------------------------")
+
+print("137.\tWinning Colors (1988)")
+print("\t\tIt's a bold move to name your horse Winning anything, but a bold move that pays off when she actually does.")
+print("136.\tFerdinand (1986)")
+print("\t\tHere is a cooler horse name, off the top of my head for free: Magellan. Come on, guys.")
+print("135.\tClyde Van Dusen (1929)")
+print("\t\tThis horse was named after his trainer. To make things even more confusing, his maternal grandfather's name was Uncle.")
